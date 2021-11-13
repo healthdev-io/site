70a5de47a376fd0b7b4bba37e1dfe1c475e0bc03
pedrolauro15 <pedrolauro15@gmail.com>
2021-11-12 22:28:39 -0300

    Ajuste no HTML da secao home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -9,6 +9,11 @@ const Home: React.FC = () => {
         <h1>
           Tecnologia, <br /> eficiência e <strong>saúde</strong>
         </h1>
+        <p>
+          Bem-vindo a HealthDev, uma startup com um único objetivo: <br />{" "}
+          <strong>melhorar a condição de saúde para todos.</strong>
+        </p>
+        <a href="#AboutUs">Arrow down</a>
       </div>
       <img src={doctor} alt="Doctor" />
     </div>
